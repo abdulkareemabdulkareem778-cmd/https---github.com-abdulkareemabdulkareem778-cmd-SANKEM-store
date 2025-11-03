d18f34eb9a74c2afbb98759bc0671d18e9137890
Abdulkareem Abdulkareem <abdulkareemabdulkareem778@gmail.com>
2025-11-03 21:56:52 +0300

    ffgdgdhfy09eyfu

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <WebView
+      source={{ uri: 'https://abdulkareemabdulkareem778-cmd.github.io/SANKEM-store/shop.html' }}
+      style={{ flex: 1 }}
+    />
+  );
+}
